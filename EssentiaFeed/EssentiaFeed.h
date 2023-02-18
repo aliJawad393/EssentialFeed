7b2a9b470a7d6d1b0fb6a087b50fdce417e21405
Ali Jawad <a.jawad@picasse.com>
2023-02-18 16:52:36 +0500

    Initial Commit

---
+++ EssentiaFeed/EssentiaFeed.h
@@ -0,0 +1,18 @@
+//
+//  EssentiaFeed.h
+//  EssentiaFeed
+//
+//  Created by Ali Jawad on 18/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentiaFeed.
+FOUNDATION_EXPORT double EssentiaFeedVersionNumber;
+
+//! Project version string for EssentiaFeed.
+FOUNDATION_EXPORT const unsigned char EssentiaFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentiaFeed/PublicHeader.h>
+
+
