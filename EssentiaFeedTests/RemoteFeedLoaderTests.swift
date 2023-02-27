085d8a8ad65d47afa21591a36198ae5dc5b215fe
Ali Jawad <a.jawad@picasse.com>
2023-02-27 08:31:52 +0500

    Add more test assertions for different HTTP code

--- EssentiaFeedTests/RemoteFeedLoaderTests.swift
+++ EssentiaFeedTests/RemoteFeedLoaderTests.swift
@@ -47,19 +47,37 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(capturedError, [.connectivity])
     }
     
+    func test_load_deliversErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+        let samples = [199, 201, 300, 500, 500].enumerated()
+        samples.forEach { index, code in
+            var capturedError = [RemoteFeedLoader.Error]()
+             sut.load {  capturedError.append($0)}
+
+            client.complete(withStatusCode: code, at: index)
+            XCTAssertEqual(capturedError, [.invalidData])
+        }
+    }
+    
     //MARK: - Helpers
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs: [URL] {
             messages.map {$0.url}
         }
-        var messages = [(url: URL, completion: (Error) -> Void)]()
-        func get(from url: URL, completion: @escaping(Error) -> Void) {
+        var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+        func get(from url: URL, completion: @escaping(Error?, HTTPURLResponse?) -> Void) {
             messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error)
+            messages[index].completion(error, nil)
+        }
+        
+        func complete(withStatusCode: Int, at index: Int = 0) {
+            let response = HTTPURLResponse(url: requestedURLs[index], statusCode: withStatusCode, httpVersion: nil, headerFields: nil)
+            
+            messages[index].completion(nil, response)
         }
     }
     
